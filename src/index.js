614f64d5a542c5942866eac291015892837b0734
agftech <agiraof@gmail.com>
2020-10-01 17:13:26 -0300

    :package: feat: create Home.view

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-function App() {
-	return <div>Olá, Mundo!!!</div>;
-}
+import HomeView from "./views/Home.view";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<HomeView />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
---
+++ src/views/Home.view.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeView() {
+	return <div>todo: home view</div>;
+}
+
+export default HomeView;
