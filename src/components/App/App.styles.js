4194dc936c6d534135e8677247503e4cfce665c4
agftech <agiraof@gmail.com>
2020-10-02 15:01:33 -0300

    :dizzy: style: add App background

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import bg from "../../assets/bg.svg";
 
 export const Wrapper = styled.div`
+	height: 100vh;
 	color: coral;
-	background-color: peachpuff;
+	background-color: #f8f5ec;
+	background-image: url(${bg});
 `;
