b8d9b8b70ed79bdc7b7dd03f2c4dc5ce8ceb403c
agftech <agiraof@gmail.com>
2020-10-02 15:06:52 -0300

    :dizzy: style: update styles to App

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -3,7 +3,22 @@ import bg from "../../assets/bg.svg";
 
 export const Wrapper = styled.div`
 	height: 100vh;
-	color: coral;
+	color: black;
 	background-color: #f8f5ec;
 	background-image: url(${bg});
+	background-size: cover;
+	background-position: center center;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Container = styled.div`
+	width: 734px;
+	height: 574px;
+
+	background-color: #ffffff;
+
+	box-shadow: 0 3px 10px rgba(0, 0, 0, 0.15);
+	border-radius: 20px;
 `;
